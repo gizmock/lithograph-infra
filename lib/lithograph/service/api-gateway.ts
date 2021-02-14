7c8f72bd7c01e89fa9999d274a25c00762f95949
kent-hamaguchi <ismism0@gmail.com>
2021-02-14 20:29:22 +0900

    rename

--- lib/lithograph/service/api-gateway.ts
+++ lib/lithograph/service/api-gateway.ts
@@ -3,16 +3,20 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigatewayv2 from "@aws-cdk/aws-apigatewayv2";
 import * as apigatewayv2Integrations from "@aws-cdk/aws-apigatewayv2-integrations";
 
-type RenderAPIProps = {
-  readonly handler: lambda.Function;
+export class ServiceRenderAPIGateway {}
+
+type Props = {
+  handler: lambda.Function;
 };
 
 export class RenderAPI extends apigatewayv2.HttpApi {
-  constructor(scope: cdk.Construct, id: string, props: RenderAPIProps) {
+  constructor(scope: cdk.Construct, id: string, props: Props) {
     super(scope, id, {});
+
     const integration = new apigatewayv2Integrations.LambdaProxyIntegration({
       handler: props.handler,
     });
+
     this.addRoutes({
       path: "/",
       methods: [apigatewayv2.HttpMethod.GET],
